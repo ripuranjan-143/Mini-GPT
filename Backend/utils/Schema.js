0c7d17f2217b115712100778d8847ce6bd35daf6
ripuranjan-143 <ripuranjan143@gmail.com>
2025-09-28 08:23:05 +0530

    update signup and login schema

--- Backend/utils/Schema.js
+++ Backend/utils/Schema.js
@@ -1,14 +1,18 @@
 import Joi from 'joi';
 
 const signupSchema = Joi.object({
-  username: Joi.string().min(3).max(25).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(4).required(),
+  body: Joi.object({
+    username: Joi.string().min(3).max(25).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(4).required(),
+  }),
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(4).required(),
-})
+  body: Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(4).required(),
+  }),
+});
 
 export default { signupSchema, loginSchema };
