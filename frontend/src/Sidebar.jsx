d061a44c6101439e618f7954f3c6700e81672913
ripuranjan-143 <ripuranjan143@gmail.com>
2025-10-09 01:50:58 +0530

    sidebar top, new chat, search, chats basic

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,9 +2,38 @@ import './Sidebar.css';
 
 const Sidebar = () => {
   return (
-    <>
-      <div>Sidebar</div>
-    </>
+    <section>
+      <div className="sidebar">
+        <div className="icon-row">
+          <img
+            src="src/assets/blacklogo.png"
+            alt="gpt logo"
+            className="logo"
+          ></img>
+          <i className="fa-solid fa-toggle-on toggle-icon"></i>
+        </div>
+
+        <div className="sidebar-body">
+          <ul className="sidebar-icons">
+            <li className="sidebar-icon mt-3 mb-2">
+              <i className="fa-solid fa-pen-to-square"></i>&nbsp; New
+              chat
+            </li>
+            <li className="sidebar-icon">
+              <i className="fa-solid fa-magnifying-glass"></i>&nbsp;
+              Search chats
+            </li>
+          </ul>
+
+          <ul className="history">
+            <p className="mt-5 ms-2 ps-1">Chats</p>
+            <li className='m-1 p-2 history-li'>
+              basic messages<i className="fa-solid fa-trash"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 };
 
